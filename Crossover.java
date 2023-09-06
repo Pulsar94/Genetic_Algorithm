ccf715d570e7f178e1428dd6af961912c185a157
AxelLns <axel.loones@gmail.com>
2023-09-06 11:02:06 -0400

    QoL improvements

--- Crossover.java
+++ Crossover.java
@@ -4,8 +4,9 @@ public class Crossover {
     public String crossover(String binaryGenes1, String binaryGenes2) {
         String crossoverGenes = "";
         Random rand = new Random();
-        int crossoverPoint = rand.nextInt(binaryGenes1.length() + 1);
+        int crossoverPoint = rand.nextInt(binaryGenes1.length() + 1); // +1 to include the last index
         System.out.println("Crossover point: " + crossoverPoint);
+        // To get the new genes
         crossoverGenes = binaryGenes1.substring(0, crossoverPoint) + binaryGenes2.substring(crossoverPoint);
         return crossoverGenes;
     }
