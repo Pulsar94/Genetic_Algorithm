f8e86a8963e6cc862bf4b1c3a53a883667c789e5
mathi <mathisg179@gmail.com>
2023-09-06 12:13:26 -0400

    projet fini

--- Main.java
+++ Main.java
@@ -1,17 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
-        Individuals[] pop = new Individuals[5];
+    public static void main(String args[]) {
+        System.out.println("Enter the number of individuals in the population");
+        Scanner sc = new Scanner(System.in);
+        int nbpop = sc.nextInt();
 
-        for (int i=0;i<5;i++){
-            pop[i]= new Individuals();
+        Individuals[] pop = new Individuals[nbpop];
+        for (int i = 0; i < nbpop; i++) {
+            pop[i] = new Individuals();
         }
-        Individuals[] fin;
-        fin=Selection.selection(pop,5);
-        for(int i=0;i<5;i++){
-            System.out.println(pop[i].getDecimalGenes()+"  "+pop[i].getFitness());
-        }
-        System.out.println(fin[0].getDecimalGenes()+" // "+fin[1].getDecimalGenes());
 
+        //display pop
+        for (int i = 0; i < nbpop; i++) {
+            System.out.println(pop[i].getDecimalGenes());
+        }
+        Individuals[] best;
+        String newpop;
+        int[] counter = {0, 0};
+
+        best = Selection.selection(pop, nbpop);
+        //change this do while loop to a while loop
+        while (best[0].getFitness() > 0 && best[1].getFitness() > 0) {
+            best = Selection.selection(pop, nbpop);
+            Random rand = new Random();
+            if (rand.nextInt(10) < 3) {
+                //System.out.println("Mutation");
+                Mutation mut = new Mutation();
+                newpop = mut.mutation(best[0].getBinaryGenes(), 1);
+                counter[0]++;
+            } else {
+                //System.out.println("Crossover");
+                Crossover cross = new Crossover();
+                newpop = cross.crossover(best[0].getBinaryGenes(), best[1].getBinaryGenes());
+                counter[1]++;
+            }
+            pop[0] = best[0];
+            pop[1] = best[1];
+            pop[2] = new Individuals(newpop, 2);
+            nbpop = 3;
+
+        }
+
+        System.out.println("The best individual is: " + pop[0].getDecimalGenes() + " and his fitness score is: " + pop[0].getFitness());
+        System.out.println("The number of mutation is: " + counter[0]);
+        System.out.println("The number of crossover is: " + counter[1]);
     }
 }
